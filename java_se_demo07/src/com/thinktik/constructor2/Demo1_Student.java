11a4cc2383ff80a0e2b81c8b0fd08aec37a305d8
thinktik <thinktik@outlook.com>
2018-01-15 20:51:32 +0800

    java se demo07

---
+++ java_se_demo07/src/com/thinktik/constructor2/Demo1_Student.java
@@ -0,0 +1,22 @@
+package com.thinktik.constructor2;
+
+class Demo1_Student {
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.show();
+	}
+}
+
+class Student {
+	private String name = "����";
+	private int age = 23;
+
+	public Student() {
+		name = "����";
+		age = 24;
+	}
+
+	public void show() {
+		System.out.println(name + "..." + age);
+	}
+}
