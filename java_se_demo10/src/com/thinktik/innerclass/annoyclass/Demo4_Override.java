2952212a4a2a0b38f5a36294a6059c2505b82c2c
thinktik <thinktik@outlook.com>
2018-01-15 21:33:35 +0800

    java se demo10

---
+++ java_se_demo10/src/com/thinktik/innerclass/annoyclass/Demo4_Override.java
@@ -0,0 +1,32 @@
+package com.thinktik.innerclass.annoyclass;
+
+class Demo4_Override {
+	public static void main(String[] args) {
+		
+	}
+}
+
+class Person {
+	public void print() {
+		System.out.println("Person");
+	}
+}
+
+class Student extends Person {
+	public void print() {
+		System.out.println("Student");
+	}
+}
+
+class Father { 
+	public Person method() {
+		return new Person();
+	}
+}
+
+class Son extends Father {
+	public Student method() {
+		return new Student();
+	}
+}
+
